7d0f05270acf985d52d6acd6a2768285799794ba
RyanCFrancis <ryan8francis@gmail.com>
2026-03-07 14:46:00 -0500

    fixed models dupe

--- models/models.go
+++ models/models.go
@@ -30,52 +30,6 @@ type EdgeWeights struct {
 
 // ---------------- Matrix Aggregator Models  ----------------
 
-// EdgeWeights stores the graph data for one transportation type.
-// Nodes is the ordered list of addresses used by the matrices.
-// Durations and Distances are directed matrices, meaning A->B can differ from B->A.
-// CombinedMatrices is the final output of the matrix aggregator.
-
-// For every directed edge from one stop to another:
-// - TimeMinutes[from][to] tells us how long the chosen mode takes
-// - CostCents[from][to] tells us the estimated cost of the chosen mode in cents
-// - Mode[from][to] tells us which transportation type was selected
-type CombinedMatrices struct {
-	TimeMinutes [][]int
-	CostCents   [][]int
-	Mode        [][]string
-}
-
-// CombineConfig controls how the aggregator compares one transportation mode
-// against another.
-
-// Notes:
-//   - Money is stored in cents instead of dollars
-//   - We compare edges using a "lower is better" value called comparison penalty:
-//     comparisonPenalty = costCents + (TimeValueCentsPerMinute * durationMinutes)
-//   - Walking is still free, walking cap to avoid choosing unrealistically
-//     long walking edges.
-type CombineConfig struct {
-	// TimeValueCentsPerMinute is the tunable tradeoff knob between time and money.
-	// Higher values make faster routes more attractive.
-	TimeValueCentsPerMinute int
-	// Walking caps. If either cap is exceeded, walking is considered invalid
-	// for that edge in .
-	WalkingMaxMinutes        int
-	WalkingMaxDistanceMeters int
-	// Subway cost model (assumption: flat fare per edge).
-	SubwayFlatFareCents int
-	// Car cost model.
-	// We now have both duration and distance available from EdgeWeights,
-	// so car pricing can use both.
-	CarBaseFareCents         int
-	CarCostPerMinuteCents    int
-	CarCostPerKilometerCents int
-	Durations                [][]int //travel times both ways: A -> B as well as B -> A, since they may differ
-	Distances                [][]int //distances both ways: A -> B as well as B -> A, since they may differ
-}
-
-// ---------------- Matrix Aggregator Models  ----------------
-
 // CombinedMatrices is returned by the matrixAggregator.
 //
 // Interpretation:
